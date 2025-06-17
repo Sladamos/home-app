3f40a3c47e69656621e697e846d286f3f9b0419e
Sladamos <sladamo@wp.pl>
2025-06-17 10:21:51 +0200

    Book entity modified

--- home-app-books/src/main/java/com/sladamos/book/Book.java
+++ home-app-books/src/main/java/com/sladamos/book/Book.java
@@ -1,15 +1,14 @@
 package com.sladamos.book;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,16 +28,31 @@ public class Book {
     @Pattern(regexp = "\\d{10}|\\d{13}", message = "ISBN must have 10 or 13 digits")
     private String isbn;
 
-    private String publisher;
-
     @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
+
+    private String publisher;
+
+    private String borrowedTo;
 
     @PositiveOrZero(message = "Number of pages cannot be negative")
     private Integer pages;
 
+    @Min(1)
+    @Max(5)
+    private Integer rating;
+
     @Lob
     private byte[] coverImage;
+
+    private Instant creationDate;
+
+    private Instant modificationDate;
+
+    private LocalDate readDate;
+
+    @Enumerated(EnumType.STRING)
+    private BookStatus status;
 
     @ElementCollection
     @CollectionTable(name = "book_authors", joinColumns = @JoinColumn(name = "book_id"))
---
+++ home-app-books/src/main/java/com/sladamos/book/BookStatus.java
@@ -0,0 +1,9 @@
+package com.sladamos.book;
+
+public enum BookStatus {
+    ON_SHELF,
+    WANT_TO_READ,
+    CURRENTLY_READING,
+    FINISHED_READING,
+    BORROWED,
+}
