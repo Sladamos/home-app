525c4d13936db44d7b48981ca5d8b59e99ce8a27
Sladamos <sladamo@wp.pl>
2025-06-22 00:31:37 +0200

    Books UI updated

--- home-app-books-desktop/src/main/java/com/sladamos/book/app/items/BookItemController.java
+++ home-app-books-desktop/src/main/java/com/sladamos/book/app/items/BookItemController.java
@@ -3,7 +3,6 @@ package com.sladamos.book.app.items;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,19 +13,13 @@ public class BookItemController {
     @FXML
     private Label titleLabel;
     @FXML
-    private Label isbnLabel;
-    @FXML
-    private Text descriptionText;
+    private Label descriptionLabel;
     @FXML
     private Label publisherLabel;
-    @FXML
-    private Label borrowedToLabel;
     @FXML
     private Label pagesLabel;
     @FXML
     private Label ratingLabel;
-    @FXML
-    private Label favoriteLabel;
     @FXML
     private Label statusLabel;
     @FXML
@@ -39,13 +32,10 @@ public class BookItemController {
     @FXML
     public void initialize() {
         titleLabel.textProperty().bind(viewModel.getTitle());
-        isbnLabel.textProperty().bind(viewModel.getIsbn());
-        descriptionText.textProperty().bind(viewModel.getDescription());
+        descriptionLabel.textProperty().bind(viewModel.getDescription());
         publisherLabel.textProperty().bind(viewModel.getPublisher());
-        borrowedToLabel.textProperty().bind(viewModel.getBorrowedTo());
         pagesLabel.textProperty().bind(viewModel.getPages().asString());
         ratingLabel.textProperty().bind(viewModel.getRating().asString());
-        favoriteLabel.textProperty().bind(viewModel.getFavorite().asString());
         statusLabel.textProperty().bind(viewModel.getStatus());
         authorsLabel.textProperty().bind(viewModel.getAuthors());
         genresLabel.textProperty().bind(viewModel.getGenres());
