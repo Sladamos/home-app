f3863a6a2fbe80aaa5e7404430bd7699ecf75562
Sladamos <sladamo@wp.pl>
2025-06-23 01:18:38 +0200

    Multilanguage support and finished titlebar

--- home-app-books-desktop/src/main/java/com/sladamos/book/app/LocaleProvider.java
+++ home-app-books-desktop/src/main/java/com/sladamos/book/app/LocaleProvider.java
@@ -3,12 +3,14 @@ package com.sladamos.book.app;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
 @Getter
+@Setter
 public class LocaleProvider {
 
     private final ObjectProperty<Locale> localeProperty = new SimpleObjectProperty<>(Locale.of("pl"));
